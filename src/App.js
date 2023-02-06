fc78e960f3d69fa660e34ca9ba39fa26ffe43acc
Ziru Zou <jiyucoding@yahoo.com>
2023-02-05 19:58:41 -0500

    set states and add onClick event listener to update count state

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import React, { useState, useEffect } from "react";
 
 function App() {
+  // 1. set states
+  const [count, setCount] = useState(0)
+  const [text, setText] = useState("")
+
+
   useEffect(() => {
     console.log("useEffect called");
   });
 
   console.log("Component rendering");
 
-  return <button>Click Me</button>;
+  return (
+    <div>
+      {/* 2. ass onClick event listener to update count */}
+      <button onClick={() => setCount((count) => count + 1)}>
+        I've been clicked {count} times
+      </button>;
+    </div>
+    
+  )
 }
 
 export default App;
