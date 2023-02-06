cfc46ee271ba1afdb30153d501c89489011e6474
Ziru Zou <jiyucoding@yahoo.com>
2023-02-05 20:31:17 -0500

    add another useEffect, only show side effect when text state changes

--- src/App.js
+++ src/App.js
@@ -5,13 +5,20 @@ function App() {
   const [count, setCount] = useState(0)
   const [text, setText] = useState("")
 
+  useEffect(() => {
+    document.title = text
+  }, [text])
 
-  useEffect(() => 
-    console.log("useEffect called"),
+
+  useEffect(() => {
+    // console.log("useEffect called"),
     // 3. pass in a second arg - a dependency array
     // [count]
-    []
-  );
+    // []
+
+    // 4. reset count back to 0 after 5s
+    setTimeout(() => setCount(0), 5000)
+  }, []);
 
   console.log("Component rendering");
 
